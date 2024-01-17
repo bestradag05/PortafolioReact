650d8c47bb2639092ba88e4e6f8a32cd33dcef52
bestradag05 <bestradag05@gmail.com>
2024-01-16 23:14:24 -0500

    Add prettier

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,51 +1,51 @@
-import react from '../assets/skills/react.png';
-import node from '../assets/skills/node.png';
-import express from '../assets/skills/express.png';
-import bs from '../assets/skills/bootstrap.png';
-import mongo from '../assets/skills/mongo.png';
-import mysql from '../assets/skills/mysql.png';
+import react from "../assets/skills/react.png";
+import node from "../assets/skills/node.png";
+import express from "../assets/skills/express.png";
+import bs from "../assets/skills/bootstrap.png";
+import mongo from "../assets/skills/mongo.png";
+import mysql from "../assets/skills/mysql.png";
 
 const Skills = () => {
-    return (
-        <section className='flex flex-col justify-center items-center'>
-            <div className="">
-                <h2 className="text-3xl text-white uppercase font-semibold titulo_subrayado">My Skills</h2>
-            </div>
-            <div className="container py-8 lg:py-12 flex justify-center items-center gap-28">
-            
-                <div className="w-16 h-auto flex flex-col justify-center items-center gap-3">
-                    <img src={react} alt="JavaScript en BegCode" />
-                    <p className='text-white uppercase'>React</p>
-                </div>
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <div className="">
+        <h2 className="titulo_subrayado text-3xl font-semibold uppercase text-white">
+          My Skills
+        </h2>
+      </div>
+      <div className="container flex items-center justify-center gap-28 py-8 lg:py-12">
+        <div className="flex h-auto w-16 flex-col items-center justify-center gap-3">
+          <img src={react} alt="JavaScript en BegCode" />
+          <p className="uppercase text-white">React</p>
+        </div>
 
-                <div className="w-16 h-auto flex flex-col justify-center items-center gap-3">
-                    <img src={node} alt="JavaScript en BegCode" />
-                    <p className='text-white uppercase'>Node.js</p>
-                </div>
+        <div className="flex h-auto w-16 flex-col items-center justify-center gap-3">
+          <img src={node} alt="JavaScript en BegCode" />
+          <p className="uppercase text-white">Node.js</p>
+        </div>
 
-                <div className="w-16 h-auto flex flex-col justify-center items-center gap-3">
-                    <img src={express} alt="JavaScript en BegCode" />
-                    <p className='text-white uppercase'>Express</p>
-                </div>
+        <div className="flex h-auto w-16 flex-col items-center justify-center gap-3">
+          <img src={express} alt="JavaScript en BegCode" />
+          <p className="uppercase text-white">Express</p>
+        </div>
 
-                <div className="w-16 h-auto flex flex-col justify-center items-center gap-3">
-                    <img src={bs} alt="JavaScript en BegCode" />
-                    <p className='text-white uppercase'>Bootstrap</p>
-                </div>
+        <div className="flex h-auto w-16 flex-col items-center justify-center gap-3">
+          <img src={bs} alt="JavaScript en BegCode" />
+          <p className="uppercase text-white">Bootstrap</p>
+        </div>
 
-                <div className="w-16 h-auto flex flex-col justify-center items-center gap-3">
-                    <img src={mongo} alt="JavaScript en BegCode" />
-                    <p className='text-white uppercase'>MongoDB</p>
-                </div>
+        <div className="flex h-auto w-16 flex-col items-center justify-center gap-3">
+          <img src={mongo} alt="JavaScript en BegCode" />
+          <p className="uppercase text-white">MongoDB</p>
+        </div>
 
-                <div className="w-16 h-auto flex flex-col justify-center items-center gap-3">
-                    <img src={mysql} alt="JavaScript en BegCode" />
-                    <p className='text-white uppercase'>MySql</p>
-                </div>
-            </div>
-
-        </section>
-    );
-}
+        <div className="flex h-auto w-16 flex-col items-center justify-center gap-3">
+          <img src={mysql} alt="JavaScript en BegCode" />
+          <p className="uppercase text-white">MySql</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
