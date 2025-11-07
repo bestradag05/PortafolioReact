925863ef95b2941ac97666b9f55fb2c88319968e
bestradag05 <bestradag05@gmail.com>
2025-11-07 12:44:58 -0500

    Retraso para los skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,12 +10,25 @@ import mysql from "../assets/skills/mysql.png";
 import git from "../assets/skills/git.png";
 
 const Skills = () => {
+  const skills = [
+    { src: react, alt: "React", name: "React" },
+    { src: php, alt: "PHP", name: "PHP" },
+    { src: laravel, alt: "Laravel", name: "Laravel" },
+    { src: node, alt: "Node.js", name: "Node.js" },
+    { src: express, alt: "Express.js", name: "Express" },
+    { src: docker, alt: "Docker", name: "Docker" },
+    { src: bs, alt: "Bootstrap", name: "Bootstrap" },
+    { src: mongo, alt: "MongoDB", name: "MongoDB" },
+    { src: mysql, alt: "MySQL", name: "MySQL" },
+    { src: git, alt: "Git", name: "Git" },
+  ];
+
   return (
     <section
       id="skills"
       className="bg-skills mb-8 flex flex-col items-center justify-center pt-10 lg:h-[500px]"
     >
-      <div className="">
+      <div>
         <h2
           data-aos="fade-up"
           className="titulo_subrayado text-3xl font-semibold uppercase"
@@ -24,120 +37,22 @@ const Skills = () => {
         </h2>
       </div>
       <div className="container mt-10 flex flex-wrap items-center justify-center gap-20 px-8 py-8 lg:py-12 xl:justify-around">
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={react}
-            alt="React"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">React</p>
-        </div>
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={php}
-            alt="php"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">PHP</p>
-        </div>
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={laravel}
-            alt="Laravel"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">LARAVEL</p>
-        </div>
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={node}
-            alt="Node.js"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">Node.js</p>
-        </div>
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={express}
-            alt="Express.js"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">Express</p>
-        </div>
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={docker}
-            alt="Docker"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">Docker</p>
-        </div>
-
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={bs}
-            alt="Bootstrap"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">Bootstrap</p>
-        </div>
-
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={mongo}
-            alt="MongoDB"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">MongoDB</p>
-        </div>
-
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={mysql}
-            alt="MySQL"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">MySql</p>
-        </div>
-
-        <div
-          data-aos="fade-up"
-          className="flex h-auto w-14 flex-col items-center justify-center gap-3"
-        >
-          <img
-            src={git}
-            alt="Git"
-            className="transition-all duration-500 hover:scale-125"
-          />
-          <p className="text-xl font-semibold uppercase">Git</p>
-        </div>
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            data-aos="fade-up"
+            data-aos-delay={`${index * 100}`} // Retraso progresivo para cada skill
+            data-aos-duration="700" // Duración de la animación
+            className="flex h-auto w-14 flex-col items-center justify-center gap-3"
+          >
+            <img
+              src={skill.src}
+              alt={skill.alt}
+              className="transition-all duration-500 hover:scale-125"
+            />
+            <p className="text-xl font-semibold uppercase">{skill.name}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
